d96ce31fda5d978bbe709fe8b12b53a9fef533fe
Johnny Tardin <johnny.tardin@m4u.com.br>
2019-07-12 22:41:29 -0300

    novas aulas

--- concorrencia/aula_75/generator.go
+++ concorrencia/aula_75/generator.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"io/ioutil"
+	"net/http"
+	"regexp"
 )
 
+// https://www.youtube.com/watch?v=f6kdp27TYZs
+
+// <-chan - canal somente-leitura
+func titulo(urls ...string) <-chan string {
+	// Aqui a função retorna um canal, com os resultados.
+	// Com isso o cliente não sabe da estrutura interna encapsulando as goroutines que são executudas de forma concorrente.
+	c := make(chan string)
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			c <- r.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
+	// o channel é retornado já pronto
+	return c
+}
+
 func main() {
-	a := [5]int{}
-	fmt.Println(reflect.TypeOf(a))
-	a[0] = 200
-	fmt.Println(a)
+	// Aqui está uma mudança original do curso onde uso um slice passando para a função variática.
+	t1 := titulo([]string{"https://www.yahoo.com.br", "https://www.uol.com.br"}...)
+	t2 := titulo([]string{"https://www.microsoft.com", "http://www.google.com"}...)
 
-	b := make([]int, 10)
-	fmt.Println(reflect.TypeOf(b))
-	b[1] = 100
-	b = append(b, 9)
-	fmt.Println(b)
-
+	fmt.Println("Primeiros:", <-t1, "|", <-t2)
+	fmt.Println("Segundos:", <-t1, "|", <-t2)
 }
